84385de403ff6113cddb5af0334514e0c40e21f5
VictorBielski <32088758+VictorBielski@users.noreply.github.com>
2022-05-08 15:17:21 +0200

    Update mqtt_template.js

--- exp_1/mqtt_template.js
+++ exp_1/mqtt_template.js
@@ -1,15 +1,21 @@
 // Variabler //
+<<<<<<< Updated upstream
 let besked;
 let value = 0;
 let turned;
 let light = document.getElementById("lyskugle");
 
 //light.style.backgroundColor = "blue";
+=======
+//let light = document.getElementById("lyskugle");
+let farve = 0; // Variable - globally declared as my_mqtt_function can change it
+>>>>>>> Stashed changes
 
 function setup() {
   createCanvas(400, 400);
 }
 
+<<<<<<< Updated upstream
 function draw() {
  // background(besked);
   fill(value);
@@ -22,3 +28,23 @@ function draw() {
 
 
 
+=======
+function draw() 
+{
+  fill(farve);
+  circle(200, 200, 200, 200);
+}
+
+// funktionen "bestemmer" farven på cirklen efter telefonens "position" på X aksen.
+function deviceTurned() {
+  if (turnAxis === 'X') {
+    if (farve === 0) {
+      farve = 255;
+      sendMessage(farve);
+    } else if (farve === 255) {
+      farve = 0;
+      sendMessage(farve);
+    }
+  }
+}
+>>>>>>> Stashed changes
