09d30da8149422a7eb9d50692bee31e680aa822c
Jonas Stenholt Ibsen <joib@itu.dk>
2022-05-08 14:05:06 +0200

    Mobile value MQTT

--- exp_1/mqtt_template.js
+++ exp_1/mqtt_template.js
@@ -1,30 +1,29 @@
 // Variabler //
-let besked;
-let value = 0;
 let light = document.getElementById("lyskugle");
-
-//light.style.backgroundColor = "blue";
+let farve = 0; // Variable - globally declared as my_mqtt_function can change it
 
 function setup() {
-  createCanvas(400, 400);
+createCanvas(400,400);
 }
 
 function draw() {
  // background(besked);
-  fill(value);
+  fill(mouseX);
   circle(200, 200, 200, 200);
-  sendMessage(mouseX);
-  console.log("hej");
 }
 
+function draw() 
+{
+  //background(farve);
+  let val = mouseX;
 
-function deviceTurned() {
-  if (turnAxis === 'Y') {
-    if (value === 0) {
-      value = 255;
-    } else if (value === 255) {
-      value = 0;
-    }
+  fill(farve);
+  circle(200, 200, 200, 200);
+  
+  if(threshold(val,10))
+  {
+    sendMessage(val);
   }
-}
+  
 
+}
