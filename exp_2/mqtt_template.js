00215a557f38baa84b7c001a1e29297584d05376
VictorBielski <32088758+VictorBielski@users.noreply.github.com>
2022-05-09 10:53:52 +0200

    slider tilføjet

--- exp_2/mqtt_template.js
+++ exp_2/mqtt_template.js
@@ -6,10 +6,11 @@ let b = 0;
 
 function setup() {
 //createCanvas(400, 400);
- // colorMode(RGB, 255);
+// colorMode(RGB, 255);
   slider = createSlider(0, 255, 0);
   slider.position(10, 10);
   slider.size(200);
+  slider.addClass('sliderClass');
 }
 
 function draw() {
