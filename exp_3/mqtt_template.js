4daa52f478b5343a31aed5e7e697a35a76e0a9b0
VictorBielski <32088758+VictorBielski@users.noreply.github.com>
2022-05-10 22:35:21 +0200

    Exp 3 færdigt

--- exp_3/mqtt_template.js
+++ exp_3/mqtt_template.js
@@ -1,25 +1,34 @@
 let slider;
 let farve = 0;
 
+
 function setup() {
   createCanvas(400, 400);
   colorMode(HSB);
   slider = createSlider(0, 360, 60, 40);
   slider.position(10, 10);
   slider.style('width', '80px');
+  slider.addClass('sliderClass');
 }
 
 function draw() {
-  circle(200, 200, 200, 200);
-  fill(farve, 100, 100, 1);
-  farve = slider.value();
+  fill(farve, 100, 100);
+  circle(200, 150, 200, 200);
+  colRep();
 }
 
 function deviceTurned() {
   if (turnAxis === 'X') {
-    sendMessage(farve);
+    farve = slider.value();
   }
+  sendMessage(farve);
 }
+
+function colRep() {
+  fill(slider.value(), 100, 100, 100);
+  rect(180, 350, 40);
+}
+
 
 // 
 /*function deviceTurned() {
